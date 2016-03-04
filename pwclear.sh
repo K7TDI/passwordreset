4221c344010b0155a72b4ee53706340bce237cc0
Ryan <k7tdi@technotechnician.com>
2016-03-04 16:04:05 -0700

    Update pwclear.sh
    less automation

--- pwclear.sh
+++ pwclear.sh
@@ -4,12 +4,6 @@ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)
 sudo apt-get update
 sudo apt-get install chntpw
 sudo apt-get install expect
-read -p "Did you click on the hard drive on the left bar? Press enter to continue"
-chntpw /media/ubuntu/*/Windows/System32/config/SAM
-echo -n "Process is complete, press enter to shutdown or n+enter to cancel"
-read answer
-if [answer == 'n'] then
-	exit 1
-else
-	sudo shutdown now
-fi
+read -p "Did you click on the hard drive on the left bar? Press enter to continue "
+
+chntpw -i /media/ubuntu/*/Windows/System32/config/SAM
