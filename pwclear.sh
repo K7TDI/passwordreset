2f5e136729abf2202333c5d69b5f8fd2e1a95ca1
Ryan <k7tdi@technotechnician.com>
2016-03-04 16:06:08 -0700

    Don't need expect

--- pwclear.sh
+++ pwclear.sh
@@ -3,7 +3,6 @@ set -x
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo apt-get update
 sudo apt-get install chntpw
-sudo apt-get install expect
 read -p "Did you click on the hard drive on the left bar? Press enter to continue "
 
 chntpw -i /media/ubuntu/*/Windows/System32/config/SAM
