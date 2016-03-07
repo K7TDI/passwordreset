f221b7f7c75cb386f755fd973a3c32e248d7bf20
Ryan <k7tdi@technotechnician.com>
2016-03-07 12:51:51 -0700

    Update pwclear.sh
    unlock windows 10 partition

--- pwclear.sh
+++ pwclear.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
+sudo mkdir /media/windoze
+sudo mount -t ntfs-3g -o remove_hiberfile /dev/sda2 /media/windoze
 set -x
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo apt-get update
 sudo apt-get install chntpw
 sudo apt-get install expect
-read -p "Did you click on the hard drive on the left bar? Press enter to continue"
-chntpw /media/ubuntu/*/Windows/System32/config/SAM
-echo -n "Process is complete, press enter to shutdown or n+enter to cancel"
-read answer
-if [answer == 'n'] then
-	exit 1
-else
-	sudo shutdown now
-fi
+sudo chntpw /media/windoze/Windows/System32/config/SAM
